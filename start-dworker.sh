4ce7a56b4768e1a842f549fb3574d6b3d5c90dea
Olivier Grisel <olivier.grisel@ensta.org>
2016-05-15 23:54:11 +0200

    Back to 1 thread per CPU, 1 proc per host

--- start-dworker.sh
+++ start-dworker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 NCPUS=`python -c "import multiprocessing as mp; print(mp.cpu_count())"`
 echo "Detected $NCPUS cpus"
-exec su $BASICUSER -c "env PATH=$PATH dworker dscheduler:8786 --nthreads 1 --nprocs $NCPUS $*"
+# Alternatively start one python process per CPU
+# exec su $BASICUSER -c "env PATH=$PATH dworker dscheduler:8786 --nthreads 1 --nprocs $NCPUS $*"
+exec su $BASICUSER -c "env PATH=$PATH dworker dscheduler:8786 $*"
