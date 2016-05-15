addbb3ce0f23e2f3aa29ddc6a49c7ae20e7cd3ba
Olivier Grisel <olivier.grisel@ensta.org>
2016-05-15 19:43:04 +0200

    Configurable STATUS_ENDPOINT

--- start-dscheduler.sh
+++ start-dscheduler.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-exec su $BASICUSER -c "env PATH=$PATH dscheduler --host dscheduler --bokeh-whitelist '*' $*"
+# XXX: instead of using the default '*' host wildcard it is highly recommended
+# to configure a public DNS name for the dscheduler service endpoint,
+# preferably with a TLS certificate and define the STATUS_ENDPOINT environment
+# variable to something like "publicname:443" instead.
+STATUS_ENDPOINT="${STATUS_ENDPOINT:-"*"}"
+exec su $BASICUSER -c "env PATH=$PATH dscheduler --host dscheduler --bokeh-whitelist="$STATUS_ENDPOINT" $*"
