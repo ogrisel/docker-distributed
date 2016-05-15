190271e544463c6c540dbd08b9636a198a340d0a
Olivier Grisel <olivier.grisel@ensta.org>
2016-05-15 22:31:19 +0200

    Make dworker use 1 proc per CPU

--- start-dworker.sh
+++ start-dworker.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-exec su $BASICUSER -c "env PATH=$PATH dworker dscheduler:8786 $*"
+NCPUS=`python -c "import multiprocessing as mp; print(mp.cpu_count())"`
+echo "Detected $NCPUS cpus"
+exec su $BASICUSER -c "env PATH=$PATH dworker dscheduler:8786 --nthreads 1 --nprocs $NCPUS $*"
