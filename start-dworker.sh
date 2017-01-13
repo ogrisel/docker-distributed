08c9d2fc95a60b66530e9d1a5351a91abbcc6b09
Kyle Foreman <kfor@uw.edu>
2017-01-12 16:08:10 -0800

    update dworker to dask-worker and dscheduler to dask-scheduler

--- start-dworker.sh
+++ start-dworker.sh
@@ -3,4 +3,4 @@ NCPUS=`python -c "import multiprocessing as mp; print(mp.cpu_count())"`
 echo "Detected $NCPUS cpus"
 # Alternatively start one python process per CPU
 # exec su $BASICUSER -c "env PATH=$PATH dworker dscheduler:8786 --nthreads 1 --nprocs $NCPUS $*"
-exec su $BASICUSER -c "env PATH=$PATH dworker dscheduler:8786 $*"
+exec su $BASICUSER -c "env PATH=$PATH dask-worker dscheduler:8786 $*"
